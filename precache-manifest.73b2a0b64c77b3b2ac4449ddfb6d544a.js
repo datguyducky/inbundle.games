f5ea99fc77048c63bfc34bed4d2c55d5fe033192
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2020-05-11 18:19:59 +0200

    Updates

---
+++ precache-manifest.73b2a0b64c77b3b2ac4449ddfb6d544a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b53ad567550c5d303d89ea4909bb27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c577de52716cf127efa8",
+    "url": "/static/js/2.8ffaaca3.chunk.js"
+  },
+  {
+    "revision": "1e3bf01ccf525cade81508e70537c92f",
+    "url": "/static/js/2.8ffaaca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e195c0c20015b21b7897",
+    "url": "/static/js/main.b49df146.chunk.js"
+  },
+  {
+    "revision": "6e5402d08a3a40160838",
+    "url": "/static/js/runtime-main.e98c5589.js"
+  }
+]);
